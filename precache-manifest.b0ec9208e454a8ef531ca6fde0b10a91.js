8c231d6e9774869501caa37e56697ffd79c1a0bc
Limeapple <Kevinliu54321@gmail.com>
2020-07-17 17:24:31 -0500

    Updates

---
+++ precache-manifest.b0ec9208e454a8ef531ca6fde0b10a91.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a3a2ba22e91e7242dac6222435a9fe",
+    "url": "/UTD-website/index.html"
+  },
+  {
+    "revision": "e9f1e94862b44ade2743",
+    "url": "/UTD-website/static/css/main.a61988b8.chunk.css"
+  },
+  {
+    "revision": "67a251f03b179f7aff7d",
+    "url": "/UTD-website/static/js/2.844cafda.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/UTD-website/static/js/2.844cafda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9f1e94862b44ade2743",
+    "url": "/UTD-website/static/js/main.396cde99.chunk.js"
+  },
+  {
+    "revision": "11ed565f1b8d8be72bc1",
+    "url": "/UTD-website/static/js/runtime-main.b92ff97c.js"
+  },
+  {
+    "revision": "9b3051e3e5d84bdb3a4297816146e33e",
+    "url": "/UTD-website/static/media/image-of-utd-near-atec.9b3051e3.jpg"
+  },
+  {
+    "revision": "10ee99d61d2fed9a216d838a6d76d99b",
+    "url": "/UTD-website/static/media/student-page-background.10ee99d6.png"
+  },
+  {
+    "revision": "6fd339fb1f7243bab2183635bd5966a1",
+    "url": "/UTD-website/static/media/temoc.6fd339fb.jpg"
+  },
+  {
+    "revision": "301929d89dd686dc2a592ebbf8c2ba1a",
+    "url": "/UTD-website/static/media/utd-logo.301929d8.jpg"
+  }
+]);
